fc46c283b5c2c471e98a6143bdb23645a40e978f
Michal Černý <33428111+cernymichal@users.noreply.github.com>
2023-02-02 02:37:54 +0100

    send cutter position only on change

--- Assets/volVR/Code/Scripts/VolumeCutter.cs
+++ Assets/volVR/Code/Scripts/VolumeCutter.cs
@@ -12,33 +12,40 @@ public class VolumeCutter : MonoBehaviour {
     }
 
     private void Update() {
-        if (!cutterPlane)
+        if (!cutterPlane || !cutterPlane.transform.hasChanged)
             return;
 
         WriteRendererProps();
     }
 
     private void OnTriggerEnter(Collider other) {
-        if (other.gameObject.CompareTag("Cutter"))
-            cutterPlane = other.gameObject;
+        if (!other.gameObject.CompareTag("Cutter"))
+            return;
+
+        cutterPlane = other.gameObject;
+        WriteRendererProps();
     }
 
     private void OnTriggerExit(Collider other) {
-        if (other.gameObject == cutterPlane) {
-            cutterPlane = null;
-            WriteRendererProps();
-        }
+        if (other.gameObject != cutterPlane)
+            return;
+
+        WriteRendererProps(cutting: false);
+        cutterPlane = null;
     }
 
-    private void WriteRendererProps() {
+    private void WriteRendererProps(bool cutting = true) {
+        Debug.Log("Send position and normal");
         MaterialPropertyBlock props = new MaterialPropertyBlock();
 
         var cutterPosition = cutterPlane ? cutterPlane.transform.position : Vector3.zero;
         var cutterNormal = cutterPlane ? cutterPlane.transform.up.normalized : Vector3.zero;
 
-        props.SetVector("_CutterPosition", new Vector4(cutterPosition.x, cutterPosition.y, cutterPosition.z, cutterPlane ? 1 : 0));
+        props.SetVector("_CutterPosition", new Vector4(cutterPosition.x, cutterPosition.y, cutterPosition.z, cutting ? 1 : 0));
         props.SetVector("_CutterNormal", new Vector4(cutterNormal.x, cutterNormal.y, cutterNormal.z, 0));
 
         renderer.SetPropertyBlock(props);
+
+        cutterPlane.transform.hasChanged = false;
     }
 }
